6311b271e192002326afd82e9611b56d0342705b
Jesaya5 <schreiter.t.s99@gmail.com>
2023-10-10 14:50:42 +0200

    Python

---
+++ main.py
@@ -0,0 +1,17 @@
+name: str = input("Wie heißt du? ")
+groesse: str = input("Wie groß bist du? ")
+stadt: str = input("Woher kommst du? ")
+traum: str = input("Wohin möchtest du gerne mal reisen? ")
+sportart: str = input("Was ist deine lieblings Sportart? ")
+serie: str = input("Was ist deine lieblings Serie? ")
+essen: str = input("Was ist dein lieblings Essen? ")
+Beruf: str = input("Was ist dein Traumberuf? ")
+madlib = f" Hallo! mein Name ist {name}.Ich bin {groesse}m groß und komme aus {stadt}. Mein Traum ist es einmal nach\
+ {traum} zu reisen und dort {sportart}zu spielen. Am liebsten schaue ich {serie} und esse dabei {essen}.\
+ Als kind habe ich immer davon geträumt {Beruf} zu werden."
+
+print(madlib)
+
+
+
+
