09f4e4e06a3f60b98119ce933c48b14a5a5b5204
Jesaya5 <147500049+Jesaya5@users.noreply.github.com>
2023-10-12 14:14:25 +0200

    Madlib

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ traum: str = input("Wohin möchtest du gerne mal reisen? ")
 sportart: str = input("Was ist deine lieblings Sportart? ")
 serie: str = input("Was ist deine lieblings Serie? ")
 essen: str = input("Was ist dein lieblings Essen? ")
-Beruf: str = input("Was ist dein Traumberuf? ")
+beruf: str = input("Was ist dein Traumberuf? ")
 madlib = f" Hallo! mein Name ist {name}.Ich bin {groesse}m groß\
     und komme aus {stadt}. Mein Traum ist es einmal nach\
  {traum} zu reisen und dort {sportart}zu spielen.\
    Am liebsten schaue ich {serie} und esse dabei {essen}.\
- Als kind habe ich immer davon geträumt {Beruf} zu werden."
+ Als kind habe ich immer davon geträumt {beruf} zu werden."
 
 
 
